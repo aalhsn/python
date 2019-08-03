9dd6c55d0b18653ca4c651bea693a5e7d912d8f9
Abdullah A. Alhasan <a.a.alhasan95@gmail.com>
2019-08-03 18:16:56 +0300

    created the function and imported the date lib

---
+++ functions_task.py
@@ -0,0 +1,14 @@
+import datetime 
+
+#this code will help separate the variabels of today's date
+
+today= date.today()
+today_yr = today.year
+today_mnth = today.month
+today_day = today.day
+
+print(" Welcome to AgeGenerator!")
+
+
+def check_birthdate(yr,mnth,day):
+
