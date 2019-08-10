faa2a0c7e46c2f2ed1a4fe8103f4dc1d2acdc759
Abdullah A. Alhasan <a.a.alhasan95@gmail.com>
2019-08-10 15:57:50 +0300

    Classes task was modified and __init__ worked!

--- classes_task.py
+++ classes_task.py
@@ -4,12 +4,10 @@ import time
 
 normal_emp_list = []
 manager_list = []
-user3 = ''
-user4 = ''
 
 class Employee:
 
-	def __intit__(self,name,age,salary,emplyment_date):
+	def __init__(self,name,age,salary,emplyment_date):
 	
 		self.name = name
 		self.age = age
@@ -17,64 +15,56 @@ class Employee:
 		self.emplyment_date = emplyment_date
 
 	def __str__(self):
-		return 
+		return "Name: %s, Age: %s, Salary: %s KWD, Working for: %s year(s)" % (self.name,self.age,round(self.salary,3), self.get_working_years())
 
-	def get_working_years(self,emplyment_date):
+	def get_working_years(self):
 		today = date.today()
-		result = today.year - int(emplyment_date)
-		return result
+		return today.year - int(self.emplyment_date)
 
 class Manager(Employee):
-	def __intit__(self, name, age, salary,emplyment_date,bonus_percentage):
-		Employee.__intit__(self,name,age,salary,emplyment_date)
-
+	def __init__(self, name, age, salary,emplyment_date,bonus_percentage):
+		Employee.__init__(self,name,age,salary,emplyment_date)
 		self.bonus_percentage = bonus_percentage
 
-	def get_bonus(self,bonus_percentage,salary):
-		return round(((bonus_percentage/100) * salary),3)
+	def get_bonus(self):
+		return round(((self.bonus_percentage/100) * self.salary),3)
+
+	def __str__(self):
+		return "Name: %s, Age: %s, Salary: %s KWD, Working for: %s year(s), BONUS: %f KWD" % (self.name,self.age,self.salary, self.get_working_years(), self.get_bonus())
 
 
 def show_list(list_name):
 	if list_name == manager_list:
-		print("--- MANAGERS LIST ---")
-		for a in list_name:
-			print ("Name: {}, Age: {}, Salary: {} KWD, Working Years: {}, (BONUS: {} KWD)".format(a.name, a.age, a.salary, a.get_working_years(a.emplyment_date), a.get_bonus(a.bonus_percentage, a.salary)))
-
+		for emp in list_name:
+			print(emp)
+			print(" ")
 	else:
-		print("--- EMPLOYEES LIST ---")
-		for a in list_name:
-			print ("Name: {}, Age: {}, Salary: {} KWD, Working Years: {}".format(a.name, a.age, a.salary, a.get_working_years(a.emplyment_date)))
+		for emp in list_name:
+			print(emp)
+			print(" ")
 
 def add_emp(class_name):
 
-		emp = class_name()
+		name = input("Name: ")
+		age = int(input("Age: "))
+		salary = float(input("Salary: "))
 
-		emp.name = input("Name: ")
-		try:
-			emp.age = int(input("Age: "))
-			emp.salary = float(input("Salary: "))
-		except ValueError:
-			print("Invaild Entry")
+		if class_name == Manager:
+			bonus_percentage= int(input("Bonus Percentage (%): "))
+			emplyment_date= int(input("Empolyment Year: "))
+			print("ADDED!")
 		else:
-		
-			try:
-				if class_name == Manager:
-				 	emp.bonus_percentage= int(input("Bonus Percentage (%): "))
-				 	emp.emplyment_date= int(input("Empolyment Year: "))
-				else:
-					emp.emplyment_date= int(input("Empolyment Year: "))
-			except ValueError:
-				print("Invaild Entry")
-			else:
-				print("ADDED!")
-				if class_name == Manager:
-					manager_list.append(emp)
-				else:
-					normal_emp_list.append(emp)
+			emplyment_date= int(input("Empolyment Year: "))
+			print("ADDED!")
 
-while True:
-	try:
-		user_input = int(input("""
+		if class_name == Manager:
+			emp = class_name(name,age,salary,emplyment_date,bonus_percentage)
+			manager_list.append(emp)
+		else:
+			emp = class_name(name,age,salary,emplyment_date)
+			normal_emp_list.append(emp)
+
+print("""
 
 	SUSHI & Co. Data Entry Program
 	===============================
@@ -85,42 +75,68 @@ while True:
 	Type number 3 for.. ADD an Employee
 	Type number 4 for.. ADD a Manager
 	Type number 5 for.. EXIT PROGRAM
+	""")
 
-	...>"""))
+while True:
+	try:
+		user_input = int(input("(0) for help..>"))
 
 	except ValueError:
 		print("Invaild Entry")
 	else:
 		if user_input == 1:
+			print("""
+				**********EMPLOYEES LIST**********
+
+				""")
+			print(" ")
 			show_list(normal_emp_list)
+			print("""
+
+				Total: {} employee(s)
+				""".format(len(normal_emp_list)))
 			continue
 		elif user_input == 2:
+			print(""""
+				_________MANAGERS LIST__________
+
+				""")
 			show_list(manager_list)
+			print("""
+
+				Total: {} manager(s)
+				""".format(len(manager_list)))
 			continue
 		elif user_input == 3:
 
 			print("""
-
 				[ADDING AN EMPLOYEE]
-
 				""")
 			add_emp(Employee)
 			continue
 		elif user_input == 4:
 
 			print("""
-
 				[ADDING A MANAGER]
-
 				""")
 			add_emp(Manager)
 			continue
 		elif user_input == 5:
 			print("CLOSING PROGRAM...")
 			sys.exit(0)
+		
+		elif user_input == 0:
+
+			print("""ACTIONS AVAIABLE TO HR EMPLOYEE:
+			Type number 1 for.. SHOW all Employees 
+			Type number 2 for.. SHOW all Managers
+			Type number 3 for.. ADD an Employee
+			Type number 4 for.. ADD a Manager
+			Type number 5 for.. EXIT PROGRAM""")
+			continue
 
 		else:
 			print("""
 				Invaild input! TRY AGAIN
-
 				""")
+			continue
