e27d8ba2f0f45a435e3fc7d0e952d215aa9031c8
Abdullah A. Alhasan <a.a.alhasan95@gmail.com>
2019-08-19 10:32:46 +0300

    functions was removed and code was modifed to contain just loops!

--- loops_task.py
+++ loops_task.py
@@ -1,67 +1,55 @@
 
 import time
-import locale
 import datetime
 
-cashier_dict = {}
 cashier_list = []
+name =''
+quantity = 0
+price = 0.0
 
-def add_item(item,price,q):
-	cashier_list.append({'name': item,
-						'price': price,
-						'quantity': q})
-
-def show_receipt():
-	print("""
-
-	=========================
-	........RECEIPT..........
-	=========================
-	""")
-
-	time.sleep(2)
-
-	for item in cashier_list:
-		total_per_item = int(item['quantity']) * float(item['price'])
-		print("{} - Quantity: ({}) - Total: {} KWD".format(item['name'].upper(),item['quantity'],round(total_per_item,3)))
-
-	total = 0
-
-	for value in cashier_list:
-		total += (int(value['quantity']) * float(value['price']))
-
-	print("""
-
-	=========================
-	GRAND TOTAL: {} KWD
-	
-
-	
-
-	""".format(round(total,3)))
 
 print("""
 
 
-	MagicCo. Point of Sales (POS) 
-	DATE|TIME {}
+	XYZ Point of Sales (POS) 
+	DATE | TIME  {}
 
-                                    powered by me!
+                                    by Abdullah Alhasan!
 
 
 	     """.format(datetime.datetime.now()))
 
-
 while True:
 
-	user_item = input("Enter Item Name or ENTER 'done' when finished:  ")
-	if user_item == 'done':
+	name= input("Type Item's Name or 'done' If You Finished..>")
+
+	if name == 'done':
 		break
-	user_price = input("Enter Price:  ")
-	user_q = input("Enter Quantity:  ")
+	price = input("Type in Item's Price..>")
+	quantity = input("Type Quantity..>")
+	cashier_list.append({'name': name,
+						'price': price,
+						'quantity': quantity})
 
-	
 
-	add_item(user_item, user_price, user_q)
+print("""
 
-show_receipt()
+=========================
+........RECEIPT..........
+=========================
+""")
+
+time.sleep(1)
+
+total = 0
+for item in cashier_list:
+	total_per_item = int(item['quantity']) * float(item['price'])
+	print("{} (Price per item: {}) - Quantity: ({}) - Total: {}KWD".format(item['name'].upper(),item['price'],item['quantity'],round(total_per_item,3)))
+	total += total_per_item
+
+print("""
+
+=========================
+GRAND TOTAL: {} KWD
+
+""".format(round(total,3)))
